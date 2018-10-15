cc6779ad251c56f947dd777db2633d1f1bbfd664
Alexandr <alexandr.rogansky@gmail.com>
2018-10-15 22:48:28 +0300

    Slider has been added

---
+++ scripts/interface.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        accessibility: true,
+        adaptiveHeight: true,
+        autoplay: true,
+        arrows: true,
+        draggable: true
+    });
+});
