9cdfa86c86c4c6489e662f59037b20f39782c99c
alexandr.rogansky@gmail.com <alex19wave>
2018-10-23 20:07:12 +0300

    Basic markup for first page has been done

--- scripts/social.js
+++ scripts/social.js
@@ -2,11 +2,20 @@ $('.follow').mouseenter(
   function() {
       $('.basic-social').css('display', 'none');
       $('.social').css('display', 'block');
+      subscribeTransition()
   }
 )
     .mouseleave(
-    function() {
-        $('.basic-social').css('display', 'block');
-        $('.social').css('display', 'none');
-    }
-);
+        function() {
+            $('.basic-social').css('display', 'block');
+            $('.social').css('display', 'none');
+            subscribeTransition()
+        }
+    );
+
+function subscribeTransition() {
+    $('.follow').css('bottom', '240px');
+    setTimeout(() => {
+        $('.follow').css('bottom', '220px');
+    }, 200);
+}
