501bce4e93437baa1fc8f427ccf60eb08c93bd57
alexandr.rogansky@gmail.com <alex19wave>
2018-10-30 17:05:56 +0200

    Project data is completely gathered

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,33 @@
-$('.list-burger li').mouseenter(
-    (event) => {
-        $(event.target).find('img').css('display', 'inline-block');
-    }
-)
-    .mouseleave(
+changeImage('.list-burger li');
+changeImage('.social-bar li');
+
+function changeImage(selector) {
+    $(selector).mouseenter(
         (event) => {
-            $(event.target).find('img').css('display', 'none');
+            $(event.target).find('img').css('display', 'inline-block');
         }
-    );
+    )
+        .mouseleave(
+            (event) => {
+                $(event.target).find('img').css('display', 'none');
+            }
+        );
+}
+
+$('.toggle-cart').click(() => {
+        $('.pop-up').css('display', 'flex')
+});
+
+$('.toggle-nav').click(() => {
+        $('.wrapper-burger').css('display', 'block');
+});
+
+$('.phone').click(() => {
+    $('.pop-up-call').css('display', 'flex')
+});
+
+$('.close-window').click(() => {
+    $('.pop-up').css('display', 'none');
+    $('.pop-up-call').css('display', 'none');
+    $('.wrapper-burger').css('display', 'none');
+});
