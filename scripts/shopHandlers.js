d3ff90ca940fad2ec6206547cd0f8696a486c0d7
AlexandrBoychenko <alex19wave>
2019-04-02 14:06:29 +0300

    Slider in shop block has been fixed

--- scripts/shopHandlers.js
+++ scripts/shopHandlers.js
@@ -1,4 +1,6 @@
 let tabIdentifier;
+let prevGrand;
+let currentNumber = 0;
 
 $('#tab1').attr('checked', 'checked');
 tabIdentifier = $('.shop-btn').attr('data-check');
@@ -16,36 +18,47 @@ $('.shop-btn').click((event) => {
         });
 
     tabIdentifier = $(event.target).attr('data-check');
-
-    console.log();
+    currentNumber = 0;
 });
 
 $('.shop-item').click((event) => {
     let image = $(event.target).attr('src');
+    let grandParent = event.target.parentNode.parentNode;
+    $(prevGrand).removeAttr('selected');
+
     if (image) {
         $('.shop-photo > img').attr('src', `${image}`);
-
         $('.shop-item').css('background', 'transparent');
-        $(event.target.parentNode.parentNode).css('background', '#a6b1c1');
+        $(grandParent).css('background', '#a6b1c1');
+        $(grandParent).attr('selected', 'true');
     }
+
+    prevGrand = grandParent;
+    currentNumber = 0;
 });
 
 $('.shop-arrow').click((event) => {
-    console.log(tabIdentifier);
+    let images = $(`.${tabIdentifier}`).find('.shop-item');
+    Object.keys(images).forEach(function(item) {
+        console.log($(images[item]).find('[selected=selected]').length, parseInt(item));
+        if (images[item].attributes && images[item].attributes.selected && !currentNumber) {
+            currentNumber = item;
+        }
+    });
 
-    let images = $('.items-block').find('.shop-item');
-    let currentImage = $('.shop-photo > img').attr('src');
-    let currentImgNumber = currentImage.match(/\d+/)[0];
-    let currentNumber = parseInt(currentImgNumber);
     if (~event.target.className.indexOf('shop-forward')) {
         if (currentNumber < images.length - 1) {
             currentNumber++;
-            $('.shop-photo > img').attr('src', `${currentImage.replace(/\d+/, '' + currentNumber)}`)
         }
     } else {
         if (currentNumber >= 0) {
             currentNumber--;
-            $('.shop-photo > img').attr('src', `${currentImage.replace(/\d+/, '' + currentNumber)}`)
         }
     }
-})
+    setSrcAttr(images);
+});
+
+function setSrcAttr(images) {
+    let src = $(images[currentNumber]).find('img').attr('src');
+    $('.shop-photo > img').attr('src', src);
+}
